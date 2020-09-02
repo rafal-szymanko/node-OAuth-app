63183715f5a3b6d68292e7e016051e225fa57d19
rafal-szymanko <61496255+rafal-szymanko@users.noreply.github.com>
2020-09-02 13:32:12 +0200

    Add new user routes

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,13 +1,33 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
+
 
 router.get('/logged', (req, res) => {
-  res.render('logged');
+
+const {name, picture} = req.user._json
+  res.render('logged', {name: name, picture: picture});
 });
 
 router.get('/no-permission', (req, res) => {
   res.render('noPermission');
 });
 
+router.get('/profile', (req, res) => {
+    console.log(req.user.id)
+    if(req.user) {
+        res.render('userProfile');
+    }   else {
+        res.redirect('/no-permission')
+    }
+  });
+
+  router.get('/profile/settings', (req, res) => {
+    if(req.user) {
+        res.render('userSettings');
+    }   else {
+        res.redirect('/no-permission')
+    }
+  });
 
 module.exports = router;
